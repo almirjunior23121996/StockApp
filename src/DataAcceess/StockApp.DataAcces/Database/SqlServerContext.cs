b01e7074abd9dc9635e60f7a5ae1b95388ce4108
almir <almir.g.junior@aluno.senai.br>
2023-12-01 21:54:09 -0300

    String de conexão implementada

---
+++ src/DataAcceess/StockApp.DataAcces/Database/SqlServerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.DataAcces.Database
+{
+    internal class SqlServerContext
+    {
+        internal static string ConexaoSemBanco => @"Data Source=.\SQLEXPRESS;Initial Catalog=StockPhone;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+        internal static string ConexaoComBanco => @"Data Source=.\SQLEXPRESS;Initial Catalog=StockPhone;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+    }
+}

950cb0847475e3a382b2891f5e61e739cecc4597
almir <almir.g.junior@aluno.senai.br>
2023-12-01 21:36:39 -0300

    explicação do professor

---
+++ src/DataAcceess/StockApp.DataAcces/Repositorios/CategoriaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using StockApp.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.DataAcces.Repositorios
+{
+    internal class CategoriaRepositorio : ICategoriaRepositorio
+    {
+        public bool
+    }
+}
---
+++ src/Presentation/StockApp.WindowsForms/frmCategoria.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StockApp.WindowsForms
+{
+    partial class frmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            txtNome = new TextBox();
+            chkStatus = new CheckBox();
+            label1 = new Label();
+            btnSalvar = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnSalvar);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(chkStatus);
+            groupBox1.Controls.Add(txtNome);
+            groupBox1.Location = new Point(40, 58);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(431, 124);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cadastrar categoria";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(24, 45);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(176, 23);
+            txtNome.TabIndex = 0;
+            // 
+            // chkStatus
+            // 
+            chkStatus.AutoSize = true;
+            chkStatus.Location = new Point(216, 49);
+            chkStatus.Name = "chkStatus";
+            chkStatus.Size = new Size(54, 19);
+            chkStatus.TabIndex = 1;
+            chkStatus.Text = "Ativo";
+            chkStatus.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Nome";
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(216, 95);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(75, 23);
+            btnSalvar.TabIndex = 3;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // frmCategoria
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "frmCategoria";
+            Text = "frmCategoria";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox chkStatus;
+        private TextBox txtNome;
+        private Button btnSalvar;
+        private Label label1;
+    }
+}
